0c41abf0bec6c028c098a0029e59ed5dff29525f
Rose Bowen <rosebowen1997@gmail.com>
2024-04-30 16:08:49 +0100

    feat (album): carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,31 +1,44 @@
-import { useStore } from "@nanostores/react";
+import { useState } from "react";
 
-import type { Photo } from "../utils/sanity";
-import { selectedCarouselItem } from "../selectedCarouselItem";
 import { urlFor } from "../utils/image";
+import type { Photo } from "../utils/sanity";
 
-export function Carousel({ photos }: { photos: Photo[] }) {
-  const $selectedCarouselItem = useStore(selectedCarouselItem);
+export const Carousel = ({ photos }: { photos: Photo[] }) => {
+  const [centralPhotoIndex, setCentralPhotoIndex] = useState(0);
+
+  const handlePhotoClick = (index: number) => {
+    setCentralPhotoIndex(index);
+  };
+
   return (
-    <section className="w-full mt-auto md:mt-[30px] ">
-      <ul className="relative overflow-x-hidden snap-mandatory w-full">
-        {photos &&
-          photos.map((photo, index) => {
-            return (
-              <li key={photo.title} className="">
-                {photo.image ? (
-                  <img
-                    className="object-contain col-span-4 md:col-span-12 mx-auto mt-auto md:mt-unset max-h-[700px]"
-                    src={urlFor(photo.image).url()}
-                    alt="Cover image"
-                  />
-                ) : (
-                  <div className="post__cover--none" />
-                )}
-              </li>
-            );
-          })}
-      </ul>
-    </section>
+    <div className="grow mt-8">
+      <div className="px-16 relative overflow-hidden h-full">
+        {photos.map((photo, index) => {
+          const toRight = index > centralPhotoIndex;
+          const toLeft = index < centralPhotoIndex;
+          const isNext = index === centralPhotoIndex + 1;
+          const isPrev = index === centralPhotoIndex - 1;
+          const leftValue = toLeft ? "0%" : toRight ? "100%" : "50%";
+          const transformValue = toLeft
+            ? `translateX(calc((-100% * ${
+                isPrev ? 1 : centralPhotoIndex - index
+              }) + 30px))`
+            : toRight
+            ? `translateX(calc((100% * ${isNext ? 0 : index}) - 30px))`
+            : "translateX(-50%)";
+          return (
+            <img
+              src={urlFor(photo.image).url()}
+              className="absolute max-w-[80vw] top-0 h-full object-contain transition-all duration-500 ease-in-out cursor-pointer"
+              style={{
+                left: leftValue,
+                transform: transformValue,
+              }}
+              onClick={() => handlePhotoClick(index)}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
-}
+};
