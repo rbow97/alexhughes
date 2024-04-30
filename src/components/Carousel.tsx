4b6660e33f397d8e3258dac232ca464a34fd2950
Rose Bowen <rosebowen1997@gmail.com>
2024-04-30 16:37:05 +0100

    feat (account): updating content in timelinevertival

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import { urlFor } from "../utils/image";
 import type { Photo } from "../utils/sanity";
+import clsx from "clsx";
 
 export const Carousel = ({ photos }: { photos: Photo[] }) => {
   const [centralPhotoIndex, setCentralPhotoIndex] = useState(0);
@@ -12,7 +13,7 @@ export const Carousel = ({ photos }: { photos: Photo[] }) => {
 
   return (
     <div className="grow mt-8">
-      <div className="px-16 relative overflow-hidden h-full">
+      <div className="relative overflow-scroll snap-proximity h-full">
         {photos.map((photo, index) => {
           const toRight = index > centralPhotoIndex;
           const toLeft = index < centralPhotoIndex;
@@ -24,12 +25,17 @@ export const Carousel = ({ photos }: { photos: Photo[] }) => {
                 isPrev ? 1 : centralPhotoIndex - index
               }) + 30px))`
             : toRight
-            ? `translateX(calc((100% * ${isNext ? 0 : index}) - 30px))`
+            ? `translateX(calc((100% * ${isNext ? 0 : index - 1}) - ${
+                isNext ? "30px" : "0px"
+              }))`
             : "translateX(-50%)";
           return (
             <img
               src={urlFor(photo.image).url()}
-              className="absolute max-w-[80vw] top-0 h-full object-contain transition-all duration-500 ease-in-out cursor-pointer"
+              className={clsx(
+                "absolute max-w-[80vw] top-0 h-full object-contain transition-all duration-500 ease-in-out",
+                { "cursor-pointer": index !== centralPhotoIndex }
+              )}
               style={{
                 left: leftValue,
                 transform: transformValue,
