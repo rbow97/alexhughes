fd4423b92623f83c78367162341aac09b8442c7e
Rose Bowen <rosebowen1997@gmail.com>
2024-05-10 21:57:13 +0100

    storing data for carousel in nano stores

---
+++ src/stores/carouselStore.ts
@@ -0,0 +1,3 @@
+import { atom } from "nanostores";
+
+export const currentAlbumLengthStore = atom<number | null>(null);
--- src/stores/selectedPhotoRefStore.ts
+++ src/stores/selectedPhotoRefStore.ts
@@ -1,3 +1,10 @@
 import { atom } from "nanostores";
 
 export const selectedPhotoRef = atom<string | undefined>(undefined);
+
+export const selectedPhotoIndex = atom<number>(0);
+
+export const currentAlbumLength = atom<number | undefined>(undefined)
+export const centralPhotoIndexInCurrentAlbum = atom<number | undefined>(undefined)
+
+export const currentAlbum = atom<{albumLength: number | undefined, centralPhotoInAlbumIndex: number | undefined}>({albumLength: undefined, centralPhotoInAlbumIndex: undefined })
