740bcd5d598add526d44fb1b3ef64393fb15e4ab
Rose Bowen <rosebowen1997@gmail.com>
2024-06-19 08:13:18 +0100

    EmptyLayout and cursor dark mode fix

--- src/components/pages/photobook/LabelAndPosition.tsx
+++ src/components/pages/photobook/LabelAndPosition.tsx
@@ -5,7 +5,7 @@ export function LabelAndPosition() {
   const $currentAlbum = useStore(currentAlbum);
 
   return (
-    <div className="mt-[30px] flex justify-between p-4 md:p-7">
+    <div className="mt-auto flex justify-between p-4 md:p-7">
       <h2 className="text-[18px] md:text-[32px]">{$currentAlbum.albumName}</h2>
       <p className="flex gap-7 text-[18px] md:gap-14 md:text-[32px]">
         <span>{$currentAlbum.centralPhotoInAlbumIndex}</span>{' '}
